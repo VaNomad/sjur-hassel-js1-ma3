2c8b2ea553c81d60865f996e194ab54e142dce9a
VaNomad <sjur.hassel@gmail.com>
2022-04-23 18:05:58 +0100

    Got key and set up url's

---
+++ js/question-2.js
@@ -0,0 +1,4 @@
+const url = "https://rawg.io";
+const corsUrl = "https://api.rawg.io/api/games?dates=2019-01-01,2019-12-31&ordering=-rating&key=f4d4f1371ed24753bc791b5cc4855b48"
+
+// key: "f4d4f1371ed24753bc791b5cc4855b48"
