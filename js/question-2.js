ce13de14201b0b8d54983ed8574121d738fa75fa
VaNomad <sjur.hassel@gmail.com>
2022-04-23 18:47:27 +0100

    Started on async function

--- js/question-2.js
+++ js/question-2.js
@@ -1,4 +1,19 @@
-const url = "https://rawg.io";
-const corsUrl = "https://api.rawg.io/api/games?dates=2019-01-01,2019-12-31&ordering=-rating&key=f4d4f1371ed24753bc791b5cc4855b48"
+const url = "https://api.rawg.io/api/games?dates=2019-01-01,2019-12-31&ordering=-rating&key=f4d4f1371ed24753bc791b5cc4855b48"
 
-// key: "f4d4f1371ed24753bc791b5cc4855b48"
+const resultBox = document.querySelector(".box");
+
+async function apiCall() {
+  try {
+    const response = await fetch(url);
+
+    const result = await response.json();
+
+    console.log(result);
+    
+  } catch (error) {
+    console.log(error);
+    resultBox.innerHTML = error
+  }
+}
+
+apiCall();
