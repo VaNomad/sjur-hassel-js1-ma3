11cebf639502e859d9f9918dd644017def334033
VaNomad <sjur.hassel@gmail.com>
2022-04-23 21:53:45 +0100

    put break, array property + assigned to innerHTML

--- js/question-2.js
+++ js/question-2.js
@@ -1,17 +1,29 @@
 const url = "https://api.rawg.io/api/games?dates=2019-01-01,2019-12-31&ordering=-rating&key=f4d4f1371ed24753bc791b5cc4855b48"
 
-const resultBox = document.querySelector(".box");
+const resultBox = document.querySelector(".listBox");
 
 async function apiCall() {
   try {
-    for (i = 0; i < result.length; i++) {
-      const response = await fetch(url);
-
-      const result = await response.json();
-
-      console.log(result);
-    }
     
+    const response = await fetch(url);
+
+    const results = await response.json();
+
+    const object = results;
+
+    console.log(results.results);
+    
+    resultBox.innerHTML = "";
+
+    for (i = 0; i < object.length; i++) { 
+      console.log(object[i]);
+
+      if (i === 8) {
+        break;
+      }
+      
+      resultBox.innerHTML += `<div class="result">${object[i].name} + ${object[i].rating} + ${object[i].tag.length}</div>`;
+    }
     
   } catch (error) {
     console.log(error);
